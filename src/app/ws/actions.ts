1d5a9c6db7ff37f922c383b535d74b9a82a2a186
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-06 17:54:20 +0600

    ✨ feat: handle auth err

--- src/app/ws/actions.ts
+++ src/app/ws/actions.ts
@@ -1,37 +1,46 @@
-"use server"
+"use server";
 
-import { db } from "@/db/drizzle"
-import { WorkspaceForm } from "./ws-dialog"
-import { workSpace } from "@/db/schema"
-import { eq } from "drizzle-orm"
-import { auth } from "@clerk/nextjs/server"
-import { revalidatePath } from "next/cache"
-
-const userId = auth().userId;
+import { db } from "@/db/drizzle";
+import { WorkspaceForm } from "./ws-dialog";
+import { workSpace } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export const createWorkSpace = async (data: WorkspaceForm) => {
-    // Add validation here
-    const existingWorkspace = await db.select().from(workSpace).where(eq(workSpace.name, data.name)).limit(1);
-    if (existingWorkspace.length) {
-        throw new Error('Workspace already exists');
-    }
+  const userId = auth().userId;
 
-    // Create workspace
-    const newWorkSpace = await db.insert(workSpace).values({
-        id: 1,
-        name: data.name,
-        creatorId: userId!,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    }).returning({
-        id: workSpace.id
+  if (!userId) {
+    throw new Error("Unauthorized access!");
+  }
+  // Add validation here
+  const existingWorkspace = await db
+    .select()
+    .from(workSpace)
+    .where(eq(workSpace.name, data.name))
+    .limit(1);
+  if (existingWorkspace.length) {
+    throw new Error("Workspace already exists");
+  }
+
+  // Create workspace
+  const newWorkSpace = await db
+    .insert(workSpace)
+    .values({
+      id: 1,
+      name: data.name,
+      creatorId: userId!,
+      createdAt: new Date(),
+      updatedAt: new Date(),
     })
+    .returning({
+      id: workSpace.id,
+    });
 
-    // Handle error 
-    if(!newWorkSpace) {
-        throw new Error("Unable to create")
-    } else {
-        revalidatePath("/ws")
-    }
-
-}
+  // Handle error
+  if (!newWorkSpace) {
+    throw new Error("Unable to create");
+  } else {
+    revalidatePath("/ws");
+  }
+};
