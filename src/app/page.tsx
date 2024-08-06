15b2aec13e1d86f0ab28f2169ae25f1903b6b182
Ashfiquzzaman Sajal <128677670+ashsajal1@users.noreply.github.com>
2024-08-06 12:29:17 +0600

    ✨ feat: query workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
-export default function Home() {
+import { db } from "@/db/drizzle";
+import { workSpace } from "@/db/schema";
+import { isNull } from "drizzle-orm";
+
+export default async function Home() {
+  const workspaces = await db.select().from(workSpace).where(isNull(workSpace.deletedAt));
+  console.log(workspaces)
   return (
     <main>
       <h1>Welcome, this is home page!</h1>
